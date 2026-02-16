a15db8487733cc3d7c8cc9f021d0d562a9a93c7d
Facundo1414 <facu.allende14@gmail.com>
2026-02-16 00:22:33 -0300

    feat: add About and Process components to Home page for improved content structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 import Services from '@/components/Services'
+import Process from '@/components/Process'
 import Testimonials from '@/components/Testimonials'
 import Footer from '@/components/Footer'
 import WhatsAppButton from '@/components/WhatsAppButton'
@@ -11,6 +13,8 @@ export default function Home() {
       <Header />
       <Hero />
       <Services />
+      <About />
+      <Process />
       <Testimonials />
       <Footer />
       <WhatsAppButton />
